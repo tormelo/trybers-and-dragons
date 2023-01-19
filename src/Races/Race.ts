2fd3e199f8515c4d7c41c807afa334f204198f43
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-19 16:13:11 -0300

    refactor Race class

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,8 +1,14 @@
 abstract class Race {
+  protected static _createdRacesInstances = 0;
+  protected _maxLifePoints = 0;
   constructor(private _name: string, private _dexterity: number) {}
   
   static createdRacesInstances() {
     throw new Error('Not implemented');
+  }
+
+  protected static addRaceInstance(): void {
+    this._createdRacesInstances += 1;
   }
   
   get name() {
