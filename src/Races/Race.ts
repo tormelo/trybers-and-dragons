bb7fccdde83ff3353e19e1d4fc067b622edb8164
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-19 15:42:39 -0300

    add Race abstract class

---
+++ src/Races/Race.ts
@@ -0,0 +1,19 @@
+abstract class Race {
+  constructor(private _name: string, private _dexterity: number) {}
+  
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+  
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
