c096a5323ee31775b353a5278b97cf84f0d637e6
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-20 16:17:58 -0300

    add Battle abstract class

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,12 +1,23 @@
-// import Fighter from '../Fighter';
+import Fighter from '../Fighter';
 
-// abstract class Battle {
-//   constructor(protected player: Fighter) { }
+abstract class Battle {
+  // 0: battling, -1: player lost, 1: player won
+  private _battleStatus = 0;
 
-//   // Should return 1 if player wins, -1 otherwise
-//   fight(): number {
-//     return this.player.lifePoints === -1 ? -1 : 1;
-//   }
-// }
+  constructor(protected player: Fighter) { }
 
-// export default Battle;
+  get battleStatus(): number {
+    return this._battleStatus;
+  }
+
+  set battleStatus(status: number) {
+    this._battleStatus = status;
+  }
+
+  // Should return 1 if player wins, -1 otherwise
+  fight(): number {
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default Battle;
